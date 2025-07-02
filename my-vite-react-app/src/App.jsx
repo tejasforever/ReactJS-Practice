207f5cbe10bc8c25b0bf991ff0a8b52c16b4145e
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-07-02 16:28:30 +0530

    npm create vite@latest my-vite-app -- --template react
    Explanation:
    
    my-vite-app = your folder name
    
    --template react = chooses React + JSX support

--- my-vite-react-app/src/App.jsx
+++ my-vite-react-app/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Home';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+       <Home />  
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
