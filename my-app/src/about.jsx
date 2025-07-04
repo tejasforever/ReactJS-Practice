00ccf9953952926a3062021e5dbbf20850891eb0
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-07-04 16:48:36 +0530

    aaaa

--- my-app/src/about.jsx
+++ my-app/src/about.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 export default function About() {
   const [number, setNumber] = React.useState(0);
-
+  let a = 0;
+  const handleClickCount = () => {
+    console.log('Button clicked');
+    a=a+1;
+    console.log(a);
+    
+  };
   const handleClick = () => {
     setNumber(number => number + 1);
   };
-  console.log(number);
+  
+  // console.log(number);
   return (
     <div>
       <h2>Number: {number}</h2>
+      <button onClick={handleClickCount}>Click Me</button>
       <button onClick={handleClick}>Increase</button>
     </div>
   );
