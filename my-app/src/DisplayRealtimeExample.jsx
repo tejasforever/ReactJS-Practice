7701d0244cbb841f0438a613f428c63924758e1f
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-07-11 16:12:50 +0530

    aaa

--- my-app/src/DisplayRealtimeExample.jsx
+++ my-app/src/DisplayRealtimeExample.jsx
@@ -1,34 +1,34 @@
 import React, { useEffect } from 'react'
 
 export default function DisplayRealtimeExample() {
-    const [data, setData] = React.useState([]);
+const [data, setData] = React.useState([]);
 
-    useEffect(() => {
-        fetch('https://6870df9c7ca4d06b34b8714d.mockapi.io/UserInformation')
-        .then(response => response.json())
-        .then(data => {
-            setData(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);
+useEffect(() => {
+fetch('https://6870df9c7ca4d06b34b8714d.mockapi.io/UserInformation')
+.then(response => response.json())
+.then(data => {
+setData(data);
+})
+.catch(error => {
+console.error('Error fetching data:', error);
+});
+}, []);
 
-  return (
-    <div>{}
-      data.map((item,index) => (
-        <div key={index}>
-            <div>
-                Name: {item.name}
-            </div>
-            <div>
-                Avatar: {item.avatar}
-            </div>
-            <div>
-                Address: {item.address}
-            </div>
+return (
+<div>{
+    data.map((item,index) => (
+    <div key={index}>
+        <div>
+            Name: {item.name}
         </div>
-      ))
+        <div>
+            Avatar: {item.avatar}
+        </div>
+        <div>
+            Address: {item.address}
+        </div>
     </div>
-  )
+    ))
+    }</div>
+)
 }
