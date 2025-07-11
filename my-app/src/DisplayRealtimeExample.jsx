b4e89e29a88f59b4d549cbd1ba91571485ad15b4
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-07-11 16:47:09 +0530

    aaa

--- my-app/src/DisplayRealtimeExample.jsx
+++ my-app/src/DisplayRealtimeExample.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect } from 'react'
+import React, {  useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function DisplayRealtimeExample() {
-const [data, setData] = React.useState([]);
+const [data, setData] = useState([]);
 
 useEffect(() => {
-fetch('https://6870df9c7ca4d06b34b8714d.mockapi.io/UserInformation')
-.then(response => response.json())
+axios.get('https://6870df9c7ca4d06b34b8714d.mockapi.io/UserInformation')
 .then(data => {
-setData(data);
+setData(data.data);
 })
 .catch(error => {
 console.error('Error fetching data:', error);
 });
 }, []);
+
+ const deleteInfo = (id) => {
+    axios.delete(`https://6870df9c7ca4d06b34b8714d.mockapi.io/UserInformation/${id}`)
+      .then(() => {
+        setData(prevData => prevData.filter(item => item.id !== id));
+      })
+      .catch(error => {
+        console.error('Error deleting user:', error);
+      });
+  };
 
 return (
 <div>{
@@ -27,6 +37,11 @@ return (
         <div>
             Address: {item.address}
         </div>
+        <div>
+            <button onClick={()=>{
+                deleteInfo(item.id);
+            }}>Delete Information</button>
+            </div>
     </div>
     ))
     }</div>
