6dbe06273a9ed05b83b267b8646ad2d441755528
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-07-15 23:21:29 +0530

    aaa

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -3,19 +3,17 @@ import LanguageDataContext from './LanguageData/LanguageData'
 
 export default function HeaderData() {
 
-  const {lan, setlan } = React.useContext(LanguageDataContext);
+  const { lan, setlan } = React.useContext(LanguageDataContext);
   const handleLanguageChange = (e) => {
     setlan(e.target.value);
   };
 
-
-
   return (
     <div>
-        <select onChange={handleLanguageChange}>
-    <option value="en">English</option>
-    <option value="hi">Hindi</option>
-  </select>
+      <select onChange={handleLanguageChange}>
+        <option value="en">English</option>
+        <option value="hi">Hindi</option>
+      </select>
     </div>
   )
 }
