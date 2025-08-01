357420ea5537d57d6ada9ab969464b2a251df312
Tejas Panchal <tejaskumar.panchal1989@gmail.com>
2025-08-01 11:36:44 +0530

    aaaa

--- react-job-portal-main/backend/app.js
+++ react-job-portal-main/backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
